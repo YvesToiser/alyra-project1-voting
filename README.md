51564ee4b5cedbfdcd32db293d5f3c24060c3412
YvesToiser <yves.toiser@gmail.com>
2022-06-13 19:59:52 +0200

    handle ex-aequo winners & reorder according to solidity style guide

--- README.md
+++ README.md
@@ -5,25 +5,27 @@ Project #1 : Voting system
 
 ### Vote description :
 
-Vote is not secret. Each voter can view the other's vote.
-Winner is determined with simple majority. proposal with the higher number of votes win.
+Vote is not secret.   
+Each voter can view the other's vote.
+Winner is determined with simple majority.   
+Proposal with the higher number of votes win.
 
 ### Requirements :
-Smart contract must be named : Voting
-Smart contract must use last compiler version.
-"Admin" is the person who will deploy the smart contract.
-Smart contract must import Ownable contract from OpenZeppelin
-Smart contract must contains following structs :
+Smart contract must be named : Voting   
+Smart contract must use last compiler version.   
+"Admin" is the person who will deploy the smart contract.   
+Smart contract must import Ownable contract from OpenZeppelin   
+Smart contract must contains following structs :   
     struct Voter {
         bool isRegistered;
         bool hasVoted;
         uint votedProposalId;
-    }
+    }   
     struct Proposal {
         string description;
         uint voteCount;
-    }
-Smart contract must contain following enum for handling different steps of the vote process:
+    }   
+Smart contract must contain following enum for handling different steps of the vote process:   
     enum WorkflowStatus {
         RegisteringVoters,
         ProposalsRegistrationStarted,
@@ -32,35 +34,39 @@ Smart contract must contain following enum for handling different steps of the v
         VotingSessionEnded,
         VotesTallied
     }    
-Smart contract must define the following events :
-    event VoterRegistered(address voterAddress); 
-    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
-    event ProposalRegistered(uint proposalId);
-    event Voted (address voter, uint proposalId);
-Smart contract must define a :
-    uint winningProposalId (the winning id) 
-    or a function getWinner which returns the winner.
+Smart contract must define the following events :   
+    event VoterRegistered(address voterAddress);    
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);   
+    event ProposalRegistered(uint proposalId);   
+    event Voted (address voter, uint proposalId);   
+Smart contract must define a :   
+    uint winningProposalId (the winning id)   
+    or a function getWinner which returns the winner.   
 
 ### Vote process
 
-Admin register a whitelist with voters Ethereum address.
-Admin starts the proposal registration session.
-Registered voters can register proposals during this session.
-Admin ends the proposal registration session.
-Admin starts the voting session.
-Voters can vote for their favorite proposal.
-Admin ends the voting session.
-Admin counts the vote.
-Everyone can see the winning proposals.
+Admin register a whitelist with voters Ethereum address.   
+Admin starts the proposal registration session.   
+Registered voters can register proposals during this session.   
+Admin ends the proposal registration session.   
+Admin starts the voting session.   
+Voters can vote for their favorite proposal.   
+Admin ends the voting session.   
+Admin counts the vote.   
+Everyone can see the winning proposals.   
 
 ### Later additions 
 
-"empty" vote must be possible. (We register it automatically as proposal #0)
-Admin can specify minimum quorum for vote validation.
-votingQuorum represents the percentage of registered voters who actually voted.
-(Number of votes / number of registered voters)
-winningQuorum represents the percentage of votes received by the winning proposal compared to number of votes.
-(Number of votes for winning proposal / number of votes)
-These 2 values are by default 0. So if the admin do no set the quorum, the vote is automatically valid.
-These values must be expressed in percentage. Ex for 50% -> input 50.
-The admin can set this quorum during the workflow status : endProposalRegistration (before the beginning of the vote)
+"empty" vote must be possible. (We register it automatically as proposal #0)   
+   
+We decided to handle the case of ex-aequo winners.   
+   
+Admin can specify minimum quorum for vote validation.   
+votingQuorum represents the percentage of registered voters who actually voted.   
+(Number of votes / number of registered voters)   
+winningQuorum represents the percentage of votes received by the winning proposal compared to number of votes.   
+(Number of votes for winning proposal / number of votes)   
+These 2 values are by default 0. So if the admin do no set the quorum, the vote is automatically valid.   
+These values must be expressed in percentage. Ex for 50% -> input 50.   
+The admin can set this quorum during the workflow status : endProposalRegistration (before the beginning of the vote)   
+Events will notify if the quorum are reached.
--- Voting.sol
+++ Voting.sol
@@ -6,10 +6,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/
 
 contract Voting is Ownable{
 
-    /**
-        These variable are specified in the specs.
-        That's why we did not use uint8.
-    */
+    // These variable are specified in the specs.
     struct Voter {
         bool isRegistered;
         bool hasVoted;
@@ -30,26 +27,22 @@ contract Voting is Ownable{
         VotesTallied
     }
 
-    uint public winningProposalId;
-
-    event VoterRegistered(address voterAddress);
-    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
-    event ProposalRegistered(uint proposalId);
-    event Voted (address voter, uint proposalId);
-    // custom events for quorum
-    event VotingQuorumEvent(bool reached);
-    event WinningQuorumEvent(bool reached);
+    // End of mandatory variables
 
     /**
         Handle the workflow status
         Automatically initialized as -> WorkflowStatus.RegisteringVoters
+        Workflow is public so the frontend can get the workflow status.
     */
     WorkflowStatus public currentStatus;
 
     // Whitelist of registered voters and array of proposals.
     // Whitelist is Public so that everybody can see the votes as per the specs.
     mapping(address => Voter) public whitelist;
-    Proposal[] public proposals;
+    Proposal[] proposals;
+    // We do arrays of winning proposals to handle case of ex-aequo.
+    uint[] winningProposalIds;
+    string[] winningProposalsDescriptions;
 
     /**
         These variables are added for quorum settlement
@@ -61,31 +54,28 @@ contract Voting is Ownable{
     uint8 registeredVoters;
     uint8 votersNb;
 
-    /**
-        This function is visible for everyone.
-    */
+    // Mandatory events
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+    event ProposalRegistered(uint proposalId);
+    event Voted (address voter, uint proposalId);
+    // custom events for quorum
+    event VotingQuorumEvent(bool reached);
+    event WinningQuorumEvent(bool reached);
 
-    function getWinner() external view returns(string memory){
-        require(currentStatus == WorkflowStatus.VotesTallied,
-            "Can not show winning proposal before closing the counts.");
-        return proposals[winningProposalId].description;
-    }
-
-    /**
-        These functions are for registered voters only.
-        They use a custom modifier onlyVoter.
-        You can register proposals or vote depending on the workflow status.
-        Specs were not very clear. As we could not contact PO for clarification,
-        we decided that a voter can register as many proposal as he wants
-        but can only vote once.
-    */
-
+    // Modifier
     modifier onlyVoter {
         require(whitelist[msg.sender].isRegistered == true,
             "You are not registered as a voter.");
         _;
     }
 
+    /**
+        These functions are for registered voters only.
+        They use a custom modifier onlyVoter.
+        You can register proposals or vote depending on the workflow status.
+        A voter can register as many proposal as he wants but can only vote once.
+    */
     function registerProposal(string calldata _description) external onlyVoter {
         require(currentStatus == WorkflowStatus.ProposalsRegistrationStarted,
             "The proposal registration period is now closed.");
@@ -108,12 +98,11 @@ contract Voting is Ownable{
     }
 
     /**
-        These functions are for admin management, therefore they have the modifier onlyowner.
+        These functions are for admin management, therefore they have the modifier onlyOwner.
         The first one add voters to the whitelist while the following ones handle the workflow.
         For convenience, we decided to handle the last step of the workflow automatically.
         This would have to be confirmed with Po in real situation.
     */
-
     function addToWhitelist(address _address) external onlyOwner {
         require(currentStatus == WorkflowStatus.RegisteringVoters,
             "Whitelist registration period is closed.");
@@ -136,6 +125,8 @@ contract Voting is Ownable{
     function endProposalRegistration() external onlyOwner {
         require(currentStatus == WorkflowStatus.ProposalsRegistrationStarted,
             "Can not end proposal registration in this workflow phase.");
+        require(proposals.length > 1,
+            "You need at least one proposal to continue the workflow.");
         currentStatus = WorkflowStatus.ProposalsRegistrationEnded;
         emit WorkflowStatusChange(WorkflowStatus.ProposalsRegistrationStarted,
             WorkflowStatus.ProposalsRegistrationEnded);
@@ -173,28 +164,41 @@ contract Voting is Ownable{
     }
 
     /**
-        Theses functions are called internally.
+        This function is visible for everyone.
+        View function must be after other external functions as per solidity style guide.
+    */
+    function getWinner() external view returns (string[] memory){
+        require(currentStatus == WorkflowStatus.VotesTallied,
+            "Can not show winning proposal before closing the counts.");
+
+        return winningProposalsDescriptions;
+    }
+
+    /**
+        These functions are called internally.
         When the admin end the voting session, it triggers the function "doCounts".
-        These function determines the winning proposal id and then trigger the function "closeCounts".
+        These function determines the winning proposal ids and then trigger the function "closeCounts".
         The last step of the workflow is then done automatically.
     */
-
     function doCounts() private {
-        uint _highestVote = proposals[0].voteCount;
-        uint8 _winId = 0;
-        for (uint8 i = 1; i < proposals.length; i++) {
-            if(proposals[i].voteCount > _highestVote){
-                _highestVote = proposals[i].voteCount;
-                _winId = i;
+        uint _highestVote = 0;
+        for (uint8 _i = 0; _i < proposals.length; _i ++) {
+            if (proposals[_i].voteCount == _highestVote){
+                winningProposalIds.push(_i);
+            } else if (proposals[_i].voteCount > _highestVote) {
+                _highestVote = proposals[_i].voteCount;
+                delete winningProposalIds;
+                winningProposalIds.push(_i);
             }
         }
-        // If 2 proposals have the same number of votes we choose the first one
-        // as this case is not handled in the specs.
-        // if the quorum are reached, we register the winning Id.
-        // else we leave the winning id at 0 which is the "empty vote"
-        if (checkQuorum(_winId)) {
-            winningProposalId = _winId;
+        for (uint8 _i = 0; _i < winningProposalIds.length; _i ++) {
+            winningProposalsDescriptions.push(proposals[winningProposalIds[_i]].description);
         }
+
+        // We will check the quorum and send related events to be handled in frontend.
+        checkQuorum(_highestVote);
+
+        // We follow through next workflow step.
         closeCounts();
     }
 
@@ -206,13 +210,10 @@ contract Voting is Ownable{
             WorkflowStatus.VotesTallied);
     }
 
-    function checkQuorum(uint8 _winId) private returns(bool){
-        bool votingQuorumReached = 100 * votersNb / registeredVoters >= votingQuorum;
-        emit VotingQuorumEvent(votingQuorumReached);
-        bool winningQuorumReached = 100 * uint8 (proposals[_winId].voteCount) / votersNb >= winningQuorum;
-        emit WinningQuorumEvent(winningQuorumReached);
-
-        return (votingQuorumReached && winningQuorumReached);
+    function checkQuorum(uint _highestVote) private  {
+        bool _votingQuorumReached = 100 * votersNb / registeredVoters >= votingQuorum;
+        emit VotingQuorumEvent(_votingQuorumReached);
+        bool _winningQuorumReached = 100 * _highestVote / votersNb >= winningQuorum;
+        emit WinningQuorumEvent(_winningQuorumReached);
     }
-
 }
